cf5820df84f30ab8f0cc83defbc69e99fafa3c52
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-05-10 15:38:12 +0700

    light main app

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+
+    # a simple page that says hello
+    @app.route('/')
+    def hello():
+        return 'Hello, World!'
+
+    return app
